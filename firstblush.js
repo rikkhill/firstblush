d74aabd454c0202310ca85676765e2817a8f9d48
Rikk Hill <rikk.hill@net-a-porter.com>
2014-06-02 16:24:16 +0100

    added toggle and jsonp example

--- firstblush.js
+++ firstblush.js
@@ -2,28 +2,34 @@
 //    v0.1
 //
 
-function clearAll() {
-    var elements = document.getElementsByClassName('firstblush_markup');
-    var replacer = [];
-    for (var i in elements) {
-        var text = elements[i].textContent;
-        replacer.push([document.createTextNode(text), elements[i]]);
-    }
-    
-    while (i = replacer.shift()) {
-    	if(typeof i[1].parentNode !== 'undefined') {
-            i[1].parentNode.replaceChild(i[0], i[1]);
-        }
-    }
-}
+
 
 (function() {
-    var i;
+
+    var script, id = "d629aa";
+
+    // Clear all of these markups
+    function clearAll() {
+        var elements = document.getElementsByClassName(id);
+        var replacer = [];
+        for (var i in elements) {
+            var text = elements[i].textContent;
+            replacer.push([document.createTextNode(text), elements[i]]);
+        }
+
+        while (i = replacer.shift()) {
+        	if(typeof i[1].parentNode !== typeof undefined) {
+                i[1].parentNode.replaceChild(i[0], i[1]);
+            }
+        }
+    }
+
     // Walk the document tree and mark up all matches to the regex `re` 
-    function markup(re, flags, color, title) {
-        var re = new RegExp(re, flags);
-        var color = color;
-        var title = title;
+    function markup(rule, class_name) {
+        var re = new RegExp(rule.pattern, rule.flags);
+        var color = rule.color;
+        var explain = rule.explain;
+        var class_name = class_name
         var i, replacer, fillers, element;
 
         var walk=document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT,null,false);
@@ -38,8 +44,8 @@ function clearAll() {
                 element = document.createElement('span');
                 element.style.backgroundColor = color;
                 element.style.cursor = 'default';
-                element.title = title;
-                element.className = "firstblush_markup";
+                element.title = explain;
+                element.className = "firstblush_markup " + class_name;
                 element.appendChild(document.createTextNode(m[0]));
                 df.appendChild(element);
             }
@@ -54,26 +60,31 @@ function clearAll() {
         }
     }
 
-    // If the ruleset doesn't exist already, create it as an empty array
-    window.ruleset = typeof window.ruleset !== 'undefined' ? window.ruleset : [];
-    
-    // Test ruleset
-    window.ruleset.push({
-    	name    : "FirstBlush",
-    	origin	: "FirstBlush Internal Example",
-    	pattern : "firstblush",
-    	flags   : "gi",
-    	color   : "#EEAAAA",
-    	title   : "FirstBlush is a splendid utility for highlighting words and phrases in your browser",
-    });
-    
-    for (i in window.ruleset) {
-        markup(
-            window.ruleset[i].pattern,
-            window.ruleset[i].flags,
-            window.ruleset[i].color,
-            window.ruleset[i].title
-        );
+    // Process json-p 
+    function blush(json) {
+        var i, return_data;
+        // Only run it if it parses as JSON
+        try {
+            return_data = JSON.parse(json);
+        } catch {
+            return_data = {};
+        }
+        // If it's got metadata, assume it's legit
+        if (typeof return_data.meta !== typeof undefined) {
+            for (i in return_data.rules) {
+                markup(return_data.rules[i], id);
+            }
+        }
     }
 
+    // Toggle highlighting based on existence of script tag
+    script = document.getElementById(id);
+    if (script.length > 0) {
+        clearAll();
+    }
+    else {
+        script = document.createElement('script');
+        script.setAttribute('src', './sanitycheck.js');
+        document.body.appendChild(script);
+    }
 })();
---
+++ sanitycheck.js
@@ -0,0 +1,42 @@
+blush({
+    meta    : {
+        name    :   "Weak Arguments",
+        blurb   :   "",
+        version :   "0.1",
+        hash    :   "e169d3",
+        author  :   "FirstBlush Ruleset Builder",
+    },
+    rules   : [
+        {
+            name    :   "Obvious",
+            pattern :   "(?:(?!not |isn't )(?:\b\w+\b|[\n.,;:]|^)(?: ){0,2})(obvious(?:ly)?)",
+            flags   :   "gi",
+            explain :   "Is it obvious?",
+            comment :   "Matches morphemes of 'obvious' not preceeded by a negative. Javascript's lack of negative look-behinds makes this surprisingly difficult.",
+            color   :   "#BBBBEE",
+        },
+        {
+            name    :   "Rather than",
+            pattern :   "(rather[^.,;:]*?than)",
+            flags   :   "gi",
+            explain :   "\"Rather [x] than [y]\" -- is this a false dichotomy?",
+            comment :   "Matches 'rather', 'than', and all intervening words if there's no intervening punctuation",
+            color   :   "#BBBBEE",
+        },
+        {
+            name    :   "Surely...",
+            pattern :   "surely",
+            flags   :   "gi",
+            explain :   "Is this perhaps at the edge of the author's sureness?",
+            color   :   "#BBBBEE",
+        },
+        {
+            name    :   "always tend to",
+            pattern :   "always tend to|tend to always",
+            flags   :   "gi",
+            explain :   "Is this trying to combine the certainty of 'always' with the hedging of 'tend to'?",
+            color   :   "#BBBBEE",
+        }
+
+    ],
+});
